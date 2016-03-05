bd43dff21ec82b2874c0ff5d6716ef30391d13c6
egorbunov <egor-mailbox@ya.ru>
2016-03-05 20:12:26 +0300

    Revert tests to initial assignment commit

--- src/test/java/ru/spbau/mit/StringSetTest.java
+++ src/test/java/ru/spbau/mit/StringSetTest.java
@@ -1,144 +1,14 @@
 package ru.spbau.mit;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
-import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class StringSetTest {
-    private static final String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    private static Random rnd = new Random();
-
-    private static String randomString(int len) {
-        StringBuilder sb = new StringBuilder(len);
-        for (int i = 0; i < len; i++)
-            sb.append(AB.charAt(rnd.nextInt(AB.length())));
-        return sb.toString();
-    }
-
-    private static Set<String> stringsDict;
-    private static Set<String> stringDictSubset; // subset of stringDict
-    private static Set<String> differentStringDict; // intersection with stringDict is empty
-    private StringSet strSet;
-
-    @Before
-    public void beforeTest() {
-        strSet = new StringSetImpl();
-    }
-
-    @BeforeClass
-    static public void prepare() {
-        final int N = 1000;
-        final int SUB_N = N / 2;
-        final int N1 = 300;
-        final int MAX_LEN = 500;
-        final int MIN_LEN = 1;
-
-        stringsDict = new HashSet<>(N);
-        stringDictSubset = new HashSet<>(SUB_N);
-        int curSubsetSize = 0;
-        for (int i = 0; i < N; i++) {
-            String str = randomString(rnd.nextInt(MAX_LEN - MIN_LEN + 1) + MIN_LEN);
-            stringsDict.add(str);
-            if (curSubsetSize++ < SUB_N) {
-                stringDictSubset.add(str);
-            }
-        }
-
-        differentStringDict = new HashSet<>(N1);
-        for (int i = 0; i < N1; ) {
-            String str = randomString(rnd.nextInt(MAX_LEN - MIN_LEN + 1) + MIN_LEN);
-            if (!stringsDict.contains(str)) {
-                i++;
-                differentStringDict.add(str);
-            }
-        }
-    }
-
-    @Test
-    public void testAdd() {
-        for (String s : stringsDict) {
-            assertTrue(strSet.add(s));
-        }
-        assertEquals(strSet.size(), stringsDict.size());
-        for (String s : stringsDict) {
-            assertTrue(strSet.contains(s));
-            assertFalse(strSet.add(s));
-        }
-    }
-
-    @Test
-    public void testContains() {
-        for (String s : stringsDict) {
-            strSet.add(s);
-        }
-        for (String s : stringDictSubset) {
-            assertTrue(strSet.contains(s));
-        }
-        for (String s : differentStringDict) {
-            assertFalse(strSet.contains(s));
-        }
-    }
-
-    @Test
-    public void testRemove() {
-        for (String s : stringsDict) {
-            strSet.add(s);
-        }
-        for (String s : stringDictSubset) {
-            assertTrue(strSet.remove(s));
-        }
-        assertEquals(strSet.size(), stringsDict.size() - stringDictSubset.size());
-        for (String s : differentStringDict) {
-            assertFalse(strSet.remove(s));
-        }
-    }
-
-    @Test
-    public void testWordsNumWithPrefix() {
-        final int TN = 50;
-        for (int i = 0; i < TN; i++) {
-            strSet = new StringSetImpl();
-            int expected = 0;
-            String prefix = randomString(rnd.nextInt(2) + 1);
-            for (String s : stringsDict) {
-                if (s.startsWith(prefix)) {
-                    expected++;
-                }
-                strSet.add(s);
-            }
-            assertEquals(expected, strSet.howManyStartsWithPrefix(prefix));
-        }
-    }
-
-    @Test
-    public void testSpecialCases1() {
-        assertEquals(strSet.size(), 0);
-        assertTrue(strSet.add(""));
-        assertTrue(strSet.remove(""));
-        assertFalse(strSet.remove("dsdsd"));
-    }
-
-    @Test
-    public void testSpecialCases2() {
-        for (String s : stringsDict) {
-            strSet.add(s);
-        }
-        assertEquals(strSet.size(), strSet.howManyStartsWithPrefix(""));
-    }
-
-
-
-    @Test(expected = Exception.class)
-    public void testBadStringAdded() {
-        strSet.add("21-392-03.,.231293-12039");
-    }
-
 
     @Test
     public void testSimple() {
@@ -153,7 +23,11 @@ public class StringSetTest {
     public static StringSet instance() {
         try {
             return (StringSet) Class.forName("ru.spbau.mit.StringSetImpl").newInstance();
-        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         throw new IllegalStateException("Error while class loading");
