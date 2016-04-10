dd75d22bd41e94c576ef2326c3c4da43ade48ef8
Egor Gorbunov <egor-mailbox@yandex.ru>
2016-04-11 02:38:48 +0300

    Add streams and stuff task solutions

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,12 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,7 +14,15 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream().map(
+                p -> {
+                    try {
+                        return Files.lines(Paths.get(p));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .flatMap(Function.identity()).filter(s -> s.contains(sequence)).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +30,25 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final long n = 500000;
+        final double r = 0.5;
+        final Random rand = new Random();
+        return Stream.generate(() -> new double[]{rand.nextDouble(), rand.nextDouble()}).limit(n)
+                .filter(p -> (p[0] - r) * (p[0] - r) + (p[1] - r) * (p[1] - r) < r * r).count() / (double) n;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
+                e -> e.getValue().stream().mapToInt(String::length).sum())).entrySet()
+                .stream().max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).orElse("");
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().map(Map::entrySet).flatMap(Set::stream)
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,103 @@
 package ru.spbau.mit;
 
+import com.google.common.collect.ImmutableMap;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import static org.junit.Assert.fail;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class SecondPartTasksTest {
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @Test
-    public void testFindQuotes() {
-        fail();
+    public void testFindQuotes() throws IOException {
+        String pattern = "world";
+        List<String> expected = Arrays.asList(
+                "Hello world!",
+                "Actually, world is a lie too.",
+                "Previous line is not blank, but world is.",
+                "World, world, world,...",
+                "Hello world, again."
+
+        );
+
+        Map<File, List<String>> files = ImmutableMap.<File, List<String>>builder()
+                .put(tmpFolder.newFile("a.txt"), Arrays.asList(
+                        expected.get(0),
+                        "Oh my darling",
+                        "Cake is a lie",
+                        expected.get(1)
+                ))
+                .put(tmpFolder.newFile("b.txt"), Arrays.asList(
+                        "This line is blank",
+                        expected.get(2),
+                        "Oh my God",
+                        "Oh my Java"
+                ))
+                .put(tmpFolder.newFile("c.txt"), Collections.emptyList())
+                .put(tmpFolder.newFile("d.txt"), Arrays.asList(
+                        expected.get(3),
+                        "Nothing to do here",
+                        expected.get(4)
+                ))
+                .build();
+
+        for (File f : files.keySet()) {
+            Files.write(f.toPath(), files.get(f));
+        }
+
+        List<String> actual = SecondPartTasks.findQuotes(
+                files.keySet().stream().map(File::getAbsolutePath).sorted().collect(Collectors.toList()), pattern);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        Assert.assertEquals(Math.PI / 4, SecondPartTasks.piDividedBy4(), 0.01);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> printers = ImmutableMap.<String, List<String>>builder()
+                .put("Pushkin", Arrays.asList("1", "1", "1"))
+                .put("Sartre", Arrays.asList("12", "12", "12"))
+                .put("Kafka", Arrays.asList("123", "1234"))
+                .build();
+        Assert.assertEquals("Kafka", SecondPartTasks.findPrinter(printers));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        List<Map<String, Integer>> orders = Arrays.asList(
+                ImmutableMap.<String, Integer>builder()
+                        .put("apple", 1).put("water", 2).put("bible", 3).build(),
+                ImmutableMap.<String, Integer>builder()
+                        .put("apple", 2).put("water", 3).put("bible", 4).build(),
+                ImmutableMap.<String, Integer>builder()
+                        .put("apple", 3).put("water", 4).put("bible", 5).build()
+        );
+
+        Map<String, Integer> expected = ImmutableMap.<String, Integer>builder()
+                .put("apple", 6).put("water", 9).put("bible", 12).build();
+
+
+        Map<String, Integer> actual = SecondPartTasks.calculateGlobalOrder(orders);
+
+        actual.keySet().stream().forEach(
+                s -> assertEquals(expected.get(s), actual.get(s))
+        );
     }
 }
