58bf19e73e4c3422823623dc6b89c3d1e0fd3863
Egor Gorbunov <egor-mailbox@yandex.ru>
2016-04-19 15:08:50 +0300

    Implement Injector #1

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,10 +1,80 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 
 public final class Injector {
+    private static HashMap<String, Object> depInstances;
+    private static HashSet<String> visited;
+
+    private static void reset() {
+        depInstances = new HashMap<>();
+        resetVisited();
+    }
+
+    private static void resetVisited() {
+        visited = new HashSet<>();
+    }
+
     private Injector() {
+    }
+
+    private static Object getDependency(Class<?> depClass, List<String> implementationClassNames) throws Exception {
+        // detecting cycle
+        if (visited.contains(depClass.getName())) {
+            throw new InjectionCycleException();
+        }
+
+        // marking, that this call is for instantiation depClass
+        visited.add(depClass.getName());
+
+        // if already instantiated, when return
+        if (depInstances.containsKey(depClass.getName())) {
+            return depInstances.get(depClass.getName());
+        }
+
+        // getting class candidates for inst
+        List<Class<?>> candidates = new ArrayList<>();
+        for (String s : implementationClassNames) {
+            Class<?> aClass = Class.forName(s);
+            if (depClass.isAssignableFrom(aClass)) {
+                candidates.add(aClass);
+            }
+        }
+
+        if (candidates.size() > 1) {
+            throw new AmbiguousImplementationException();
+        } else if (candidates.size() == 0) {
+            throw new ImplementationNotFoundException();
+        }
+
+        Class<?> forInstantiation = candidates.get(0);
+
+        // checking for constructor without parameters
+        Constructor<?> constructor = forInstantiation.getConstructors()[0];
+        Class<?>[] pTypes = constructor.getParameterTypes();
+        if (pTypes.length == 0) {
+            Object inst = constructor.newInstance();
+            depInstances.put(depClass.getName(), inst);
+            return inst;
+        }
+
+        Object[] params = new Object[pTypes.length];
+        for (int i = 0; i < params.length; i++) {
+            Class<?> type = pTypes[i];
+            params[i] = getDependency(type, implementationClassNames);
+        }
+
+        Object obj = constructor.newInstance(params);
+
+        // putting for name of initial dependency (may be interface)
+        depInstances.put(depClass.getName(), obj);
+
+        return obj;
     }
 
     /**
@@ -12,6 +82,22 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        reset();
+
+        Class<?> clazz = Class.forName(rootClassName);
+        Constructor<?> constructor = clazz.getConstructors()[0];
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+        // for preventing cycle dependencies on root class
+        visited.add(clazz.getName());
+
+        Object[] params = new Object[parameterTypes.length];
+        for (int i = 0; i < params.length; ++i) {
+            resetVisited();
+            params[i] = getDependency(parameterTypes[i], implementationClassNames);
+        }
+
+        // instantiating...
+        return constructor.newInstance(params);
     }
 }
